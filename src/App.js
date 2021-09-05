d72a8efb132e0fa207d342ca782d86b0e1ea0a0e
Saurabh <saurabhrajput651@gmail.com>
2021-09-05 18:08:51 +0530

    Loaded users using rest api

--- src/App.js
+++ src/App.js
@@ -1,35 +1,44 @@
 import React from "react";
 import CardList from "./CardList";
 import Searchbox from './Searchbox';
-import {robots} from './robots'
 import './App.css'
 
-class App extends React.Component{
-constructor(){
-    super();
-    this.state={
-        robots: robots,
-        searchfield:''
-    };
-}
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: [],
+            searchfield: ''
+        };
+    }
 
-OnSearchChanged = (event) => {
-    this.setState({searchfield: event.target.value});
-}    
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json)
+            .then(users => this.setState({ robots: users }));
+    }
 
-render(){    
-    const filterRobots = this.state.robots.filter(robot =>{
-        return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
-    });
-    return(
-    <div className='tc'>
-        <h1 className='f1'>Robofriends</h1>
-        <Searchbox searchChange={this.OnSearchChanged}/>
-        <CardList robots={filterRobots}/>
+    OnSearchChanged = (event) => {
+        this.setState({ searchfield: event.target.value });
+    }
 
-    </div>  
-   );
-}
+    render() {
+        const filterRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        });
+        if (this.state.robots.length === 0) {
+            return <h1>Loading...</h1>
+        } else {
+            return (
+                <div className='tc'>
+                    <h1 className='f1'>Robofriends</h1>
+                    <Searchbox searchChange={this.OnSearchChanged} />
+                    <CardList robots={filterRobots} />
+
+                </div>
+            );
+        }
+    }
 }
 
 export default App
